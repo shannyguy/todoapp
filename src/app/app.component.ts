7a128bb46c47df7d8e5e1f062096b6a693c18320
Guy, Shanny (sg234e) <sg234e@intl.att.com>
2018-07-22 21:12:10 +0300

    Added save in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  newTodo: Todo = new Todo();
+  priorities = Array<string>();
+
+  ngOnInit(): void {
+    
+            this.priorities = Array<string>();
+            this.priorities.push('High');
+            this.priorities.push('Normal');
+            this.priorities.push('Low');      
+        }
+      
+
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+  updateTodo(todo){
+    this.todoDataService.updateTodoById(todo.id, todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+  loadTodosFromLocalStorage(){
+    this.todoDataService.loadTodosFromLocalStorage();
+  }
+
 }
